c8f0d3d2f55715aee0639380dc526bc404f4b1a9
Karolína Božková <karolina.bozkova@gmail.com>
2017-03-26 20:50:18 +0200

    display user login in navbar instead of 'Account'

--- src/main/java/cz/tournament/control/repository/GameRepository.java
+++ src/main/java/cz/tournament/control/repository/GameRepository.java
@@ -1,10 +1,8 @@
 package cz.tournament.control.repository;
 
 import cz.tournament.control.domain.Game;
-
 import org.springframework.data.jpa.repository.*;
 
-import java.util.List;
 
 /**
  * Spring Data JPA repository for the Game entity.
