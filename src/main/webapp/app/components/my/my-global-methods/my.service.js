9cba80890e61f5d4bf2fec4a25f0c5a03540d44e
Karolína Božková <karolina.bozkova@gmail.com>
2017-11-06 13:04:47 +0100

    modified index, moved my coomponents

---
+++ src/main/webapp/app/components/my/my-global-methods/my.service.js
@@ -0,0 +1,90 @@
+(function () {
+    'use strict';
+    angular
+            .module('tournamentControlApp')
+            .factory('My', My);
+
+    My.$inject = [];
+    
+    function getParticipantName(participant) {
+        if(participant){
+            if(participant.player){
+                return participant.player.name;
+            }
+            if(participant.team){
+                return participant.team.name;
+            }
+            return 'BYE';
+        }
+        return '-';
+    }
+    
+    function contains(array, elem) {
+        for (var i = 0; i < array.length; i++) {
+            if(!elem){ // elem === null
+                if(!array[i]){
+                    return true;
+                }
+            }
+            else if (array[i] && array[i].id === elem.id){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function My() {
+        return {
+            'getParticipantName': function (participant) {
+                return getParticipantName(participant);
+            },
+            'containsElemWithId': function (array, elem) {
+                return contains(array,elem);
+            },
+            'getSeedingOptionName': function getSeedingOptionName(seeding, participant) {
+                if(participant){
+                        if(contains(seeding, participant)){
+                        return participant.name;
+                    }
+                    return participant.name +" (not seeded)";
+                }
+                return "";
+            },
+            'getN': function getN(n) {
+                if ((n & (n - 1)) === 0) {
+                return n;
+                }
+                while ((n & (n - 1)) !== 0) {
+                    n = n & (n - 1);
+                }
+                n = n << 1;
+                return n;
+            },
+            'printRivals': function printRivals(list) {
+                if(!list) return '';
+                var string = "[";
+                for (var i = 0; i < list.length; i++) {
+                    string = string.concat(getParticipantName(list[i]));
+                    if(i !== list.length - 1){
+                        string = string.concat(", ");
+                    }
+                }
+                string = string.concat("]");
+                return string;
+            },
+            'isBye': function isBye(rival){
+                if(rival){
+                    return rival.bye;
+                }
+                return false;
+            },
+            'iterator': function iterator(from, to, step){
+                var array = [];
+                for (var i = from; i < to; i+=step) {
+                    array[i]=i;
+                }
+                return array;
+            }
+        };
+    }
+})();
